3eab649bf117ee4f1625857aff6f4e37cf5b9f5a
Pablolo <pablo_olondriz@hotmail.com>
2020-03-13 19:41:32 +0100

    noidea

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,20 +1,6 @@
 const mongoose = require('mongoose');
 const Chef = require('../models/chef');
 
-// Link to connect to Database: mongodb+srv://chefapp:1234@module2project-ko7or.gcp.mongodb.net/test?retryWrites=true&w=majority
-mongoose
-	.connect('mongodb+srv://chefapp:1234@module2project-ko7or.gcp.mongodb.net/test?retryWrites=true&w=majority', {
-		useCreateIndex: true,
-		useNewUrlParser: true,
-		useUnifiedTopology: true
-	})
-	.then(x => {
-		console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-	})
-	.catch(err => {
-		console.error('Error connecting to mongo', err)
-	});
-
 const chefs = [
 	{
 		mail: 'josehernandez@gmail.com',
@@ -82,10 +68,25 @@ const chefs = [
 	}
 ];
 
+// Link to connect to Database: mongodb+srv://chefapp:1234@module2project-ko7or.gcp.mongodb.net/test?retryWrites=true&w=majority
+mongoose
+	.connect('mongodb+srv://chefapp:1234@module2project-ko7or.gcp.mongodb.net/test?retryWrites=true&w=majority', {
+		useCreateIndex: true,
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	})
+	.then(x => {
+		console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+		return Chef.insertMany(chefs);
+	})
+	.catch(err => {
+		console.error('Error connecting to mongo', err)
+	});
+
 Chef.create(chefs, err => {
 	if (err) {
 		throw err;
 	}
-	console.log(`Created ${chefs.length} movies`);
+	console.log(`Created ${chefs.length} chefs`);
 	mongoose.connection.close();
 });
