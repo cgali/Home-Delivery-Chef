e1c45a016fb338b72d71fc481577792407727e80
Pablolo <pablo_olondriz@hotmail.com>
2020-03-13 16:43:44 +0100

    create signup page

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,8 @@ router.get('/login', (req, res, next) => {
 	res.render('auth/login');
 });
 
+router.get('/signup', (req, res, next) => {
+	res.render('auth/signup');
+});
+
 module.exports = router;
