bfb3bc2c80b00a4c10c28338b072af7e22c83c7d
Pablolo <pablo_olondriz@hotmail.com>
2020-03-13 17:39:52 +0100

    cleaning code

--- routes/chefs.js
+++ routes/chefs.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET Chefs page. */
-router.get('/', function(req, res) {
+/* GET /chefs page. */
+router.get('/', (req, res) => {
   res.render('chefs');
+});
+
+/* GET /chefs/:id page. */
+router.get('/:id', (req, res) => {
+  res.render('chefs/');
 });
 
 module.exports = router;
