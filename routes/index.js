d5b521631722f0e3146a4d31acc742d541428989
carlamt25 <57832951+carlamt25@users.noreply.github.com>
2020-03-13 18:33:48 +0100

    changes seeds

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Home Delivery Chef' });
+router.get('/', function (req, res) {
+	res.render('index', { title: 'Home Delivery Chef' });
 });
 
-router.get('/login', (req, res, next) => {
+router.get('/login', (req, res) => {
 	res.render('auth/login');
 });
 
-router.get('/signup', (req, res, next) => {
+router.get('/signup', (req, res) => {
 	res.render('auth/signup');
 });
 
