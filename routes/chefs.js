9646163a52529f7b252e6416b094e632fc677e44
Pablolo <pablo_olondriz@hotmail.com>
2020-03-14 12:08:32 +0100

    trying to connect DB

--- routes/chefs.js
+++ routes/chefs.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+const Chefs = require('../models/chef');
+
 /* GET /chefs page. */
 router.get('/', (req, res) => {
-  res.render('chefs');
-});
-
-/* GET /chefs/:id page. */
-router.get('/:id', (req, res) => {
-  res.render('chefs/');
+  Chefs.find()
+  .then(chefs => {
+    res.render('chefs', { chefs });
+  })
+  .catch(err => console.log('Error while listing chefs: ', err));
+  console.log('listing chefs');
 });
 
 module.exports = router;
