3c78b159f886a9078d2635c034b0df912102c877
Carles Galí Bou <carles_ga@hotmail.com>
2020-04-01 03:40:25 +0200

    doing cart but with problems, changed link and image of 4th section in index

--- routes/cart.js
+++ routes/cart.js
@@ -4,32 +4,55 @@ const Client = require('../models/client');
 
 const router = express.Router();
 
-// GET Cart page 
-router.get('/', (req, res) => {
+// POST delete to cart
+router.post('/:id/deleteAll', (req, res) => {
 	const { currentClient } = req.session;
-	res.render('cart', { currentClient });
-});
-
-// POST add to cart
-router.post('/:id/addToCart', (req, res) => {
-	const { currentClient } = req.session;
-	const { id } = req.params;
-
-
-
-
+  const { id } = req.params;
+  
 	Client.findById(currentClient._id)
 		.then((client) => {
-      client.cart.push(id)
-      return client.save()
+      client.cart.deleteOne()
+      client.save()
     })
     .then(() => {
-      res.redirect('/cart');
+      res.redirect(`/cart/${id}`)
     })
 		.catch(() => {
 
 		})
 })
+
+// GET Cart page 
+router.get('/:id', (req, res) => {
+  const { currentClient } = req.session;
+  const { id } = req.params;
+  console.log(id)
+  Menus.findById(id)
+  .then ((menus) => {
+    console.log(menus)
+    res.render('cart', { currentClient, menus });
+  })
+	
+});
+
+// POST add to cart
+router.post('/:id/addToCart', (req, res) => {
+	const { currentClient } = req.session;
+  const { id } = req.params;
+  
+	Client.findById(currentClient._id)
+		.then((client) => {
+      client.cart.push(id)
+      client.save()
+    })
+    .then(() => {
+      res.redirect(`/cart/${id}`)
+    })
+		.catch(() => {
+
+		})
+})
+
 
 
 module.exports = router;
